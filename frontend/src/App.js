b742138c2c990c32d2611957438de540719bc631
Maazz3234 <maazsiddiqui914@gmail.com>
2023-04-27 01:31:13 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,17 @@ import Home from './components/main/Home';
 
 function App() {
   return (
-   <div>
-    <BrowserRouter>
-    
-    <Routes>
-    <Route path="main" element={<Main />}>
-    <Route path='home' element={<Home/>}/> 
-    </Route>
-    </Routes>
-    </BrowserRouter>
-   </div>
+    <div>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="main" element={<Main />}>
+            <Route path='home' element={<Home />} />
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
