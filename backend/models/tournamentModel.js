5fa4e75f608419943ac15efc4c28a820884a5360
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-13 13:50:52 +0530

    ..

--- backend/models/tournamentModel.js
+++ backend/models/tournamentModel.js
@@ -3,12 +3,15 @@ const { Schema, model } = require('../connection');
 
 const myschema = new Schema(
     {
-        title: String,
-        game: String,
+        title: { type: String, required: true, unique: true },
+        game: { type: String, required: true },
         description: String,
-        user: { type: Types.ObjectId, ref: "user" },
-        createdAt: Date,
-        updatedAt: Date,
-        image: String
+        createdBy: { type: Types.ObjectId, ref: "user" },
+        players: [{ type: Types.ObjectId, ref: "player" }],
+        tournamentMatches: Object,
+        categories: [],
+        image: String,
+        created_at: Date,
+        updated_at: Date,
     });
 module.exports = model('tournament', myschema);
